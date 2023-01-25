71c8e0165b9080840bdba03a767fd2d883cc3039
Shu Zhang <shu@broadinstitute.org>
2023-01-24 17:16:20 -0800

    add bfs and unit tests

--- search/graph.py
+++ search/graph.py
@@ -12,16 +12,101 @@ class Graph:
         """
         self.graph = nx.read_adjlist(filename, create_using=nx.DiGraph, delimiter=";")
 
+
+
+
+    def shortest_path(self, pred, start, end):
+        """
+        takes in dict of predecessors and finds shortest path
+        starts from end node and works backwards to start 
+        """
+        path_list=[end]
+        new_end=end
+
+        while new_end != start:
+            path_list.append(pred[new_end])
+            new_end=pred[new_end]
+        
+        return(path_list[::-1])
+
+
+        
+
+
+
+
+
+   
     def bfs(self, start, end=None):
         """
         TODO: write a method that performs a breadth first traversal and pathfinding on graph G
-
         * If there's no end node input, return a list nodes with the order of BFS traversal
         * If there is an end node input and a path exists, return a list of nodes with the order of the shortest path
         * If there is an end node input and a path does not exist, return None
 
         """
-        return
+
+        G=self.graph
+
+         #check that it's not an empty graph
+        if nx.is_empty(G):
+            raise ValueError(f"Graph is empty!")
+
+        #make sure to handle unconnected graph
+       
+
+        #check that start and end node exists in graph 
+
+
+
+        #lists of visted nodes and nodes to visit in queue
+        queue=[start]
+        visited=[start]
+        traversed=[start]
+
+        #dictionary of predecessors
+        pred={}
+        pred[start]=None
+
+        #loop through while queue is not empty 
+        while queue:
+            #print('current queue: ' + str(queue))
+            #pop first element of queue!!!!
+            v=queue.pop(0)
+            #print(v)
+            N=G.neighbors(v)
+            traversed.append(v)
+            
+            #print(dict(enumerate(N)))
+            
+            for w in N:
+                if w not in visited:
+                    #print('current w: ' + str(w))
+                    pred[w] = v
+                    visited.append(w)
+                    queue.append(w)
+                    #print('visted: ' + str(visited))
+
+
+        
+        if end == None:
+            #return list of nodes with order of BFS traversal
+
+            return(visited)
+
+
+        elif nx.has_path(self.graph, start, end):
+
+            #turn predecessors dict into list of shortest path 
+            return(self.shortest_path(pred, start, end))   
+
+        #has end node input but does not have path 
+        else:
+            return(None)
+
+
+
+        
 
 
 
--- test/test_bfs.py
+++ test/test_bfs.py
@@ -1,8 +1,9 @@
 # write tests for bfs
 import pytest
-from search import graph
+import networkx as nx
+from search import Graph
 
-@pytest.fixture
+
 def test_bfs_traversal():
     """
     TODO: Write your unit test for a breadth-first
@@ -11,7 +12,23 @@ def test_bfs_traversal():
     that all nodes are being traversed (ie. returns 
     the right number of nodes, in the right order, etc.)
     """
-    pass
+
+    G_tiny=Graph('./data/tiny_network.adjlist')
+    
+    #bfs with only start 
+    traversed_G_tiny=(G_tiny.bfs('Lani Wu'))
+
+    #check number of nodes traversed
+    assert len(traversed_G_tiny) == 30
+
+    #check order of nodes traversed
+    assert traversed_G_tiny 
+
+   #check correct number of nodes
+   #check node order ? 
+
+    #assert 
+    
 
 def test_bfs():
     """
@@ -24,4 +41,40 @@ def test_bfs():
     Include an additional test for nodes that are not connected 
     which should return None. 
     """
-    pass
+
+    G_cit=Graph('./data/citation_network.adjlist')
+
+    pair_list=[('Lani Wu', 'Martin Kampmann'), 
+            ('Hani Goodarzi', 'Tanja Kortemme'),
+            ('Tony Capra', 'Jimmie Ye') ]
+
+    #check shortest path against networkx shortest path 
+    for pair in pair_list:
+        #find all possible shortest paths for given pair
+        all_shortest=[x for x in nx.all_shortest_paths(G_cit.graph, pair[0], pair[1])]
+        #check that path output is one of all possible shortest
+        assert  G_cit.bfs(pair[0], pair[1]) in all_shortest, "Shortest path detected incorrect"
+    
+
+    #check if nodes unconnected
+    assert G_cit.bfs('Hao Li', 'Bruce Conklin') == None, "Unconnected nodes does not return None"
+
+
+    
+    
+
+
+
+def test_bfs_edgecase():
+
+
+    #make instance of empty graph
+
+    #make sure 
+
+
+    pass 
+#add tests for empty graphs
+#start nodes that don't exist
+#end nodes that don't exist
+#two nodes that aren't connected 
